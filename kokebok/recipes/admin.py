c7d42b719ad6cf603c03c06bfb1a58263287ff54
Mia Pernille Treland <mia.treland@gmail.com>
2023-09-25 22:45:13 +0200

    define recipe models

---
+++ kokebok/recipes/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from recipes.models import Ingredient, Recipe, RecipeIngredient
+
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeIngredient)
--- kokebok/recipes/models.py
+++ kokebok/recipes/models.py
@@ -1,11 +1,30 @@
 from django.db import models
+from django.forms import ValidationError
 
 
 class Recipe(models.Model):
-    ...
-
-class RecipeIngredient(models.Model):
-    ...
+    title = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    origin_url = models.URLField(blank=True)
 
 class Ingredient(models.Model):
-    ...
+    name_no = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    name_en = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    name_de = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    name_fr = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    name_it = models.CharField(max_length=64, unique=True, null=True, blank=True)
+    is_ubiquitous = models.BooleanField(default=False)
+
+    def clean(self):
+        if not any([self.name_no, self.name_en, self.name_de, self.name_fr, self.name_it]):
+            raise ValidationError("Ingredient must have at least one name!")
+        return super().clean()
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(to=Recipe, on_delete=models.CASCADE, related_name="recipe_ingredients")
+    base_ingredient = models.ForeignKey(to=Ingredient, on_delete=models.PROTECT, related_name="recipe_ingredients")
+    name_in_recipe = models.CharField(max_length=64)
+    is_optional = models.BooleanField(default=False)
+
